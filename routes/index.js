efaf81970abbf72f2ee4b7844164cf8b53f43f39
jjuriz <jjuriz@gmail.com>
2020-04-21 11:50:43 -0400

    Able to create new posts.

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+let express = require('express');
+let router = express.Router();
+let repo = require('../models/postRepository');
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: ' My Cool Site',
+    author: 'Jeremy Uriz',
+    dataSource: repo.dataSource,
+    postCount: repo.postCount(),
+    posts: repo.getPosts()
+  });
+});
+
+module.exports = router;
---
+++ routes/post.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let router = express.Router();
+let repo = require("../models/postRepository");
+
+router.get('/:permalink', (req, res, next) => {
+    let post = repo.getPostByPermalink(req.params.permalink);
+    res.render('post', { title: post.title, post: post});
+});
+
+
+module.exports = router;
